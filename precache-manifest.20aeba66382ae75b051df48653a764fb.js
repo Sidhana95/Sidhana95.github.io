dfe12317d50c33c8d0b8690d87df361ad20365c4
Kumar Adarsh Kanungo <kanungo.kumaradarsh@gmail.com>
2020-08-18 08:37:56 +0530

    1st revision

---
+++ precache-manifest.20aeba66382ae75b051df48653a764fb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fc80bb25645530a0b11f2046cbe3709",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02caa7eb681088d775e4",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "aeb3a18c7bfe6e641bf8",
+    "url": "/static/css/main.04638b96.chunk.css"
+  },
+  {
+    "revision": "02caa7eb681088d775e4",
+    "url": "/static/js/2.8f9c49c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f9c49c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeb3a18c7bfe6e641bf8",
+    "url": "/static/js/main.315dcae9.chunk.js"
+  },
+  {
+    "revision": "43b25eb14975ab1b1919",
+    "url": "/static/js/runtime-main.146b4aeb.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
